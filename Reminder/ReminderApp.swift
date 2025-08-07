6a73bd2c7cb1aa0e8bda7d6ae1478cd3a45017b6
Tuấngg Anhhh <atdevv2311@gmail.com>
2025-08-07 09:52:01 +0700

    Initial Commit

---
+++ Reminder/ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderApp.swift
+//  Reminder
+//
+//  Created by Tuấngg Anhhh on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
