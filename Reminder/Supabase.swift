9ae5b3e33374c06c439b2f64770af3c4d6d61899
Tuấngg Anhhh <atdevv2311@gmail.com>
2025-08-08 11:29:49 +0700

    fix: rm track Supabase file

--- Reminder/Supabase.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-import Supabase
-
-let supabase = SupabaseClient(
-    supabaseURL: URL(string: "https://qifogdoczlpjlzayidbh.supabase.co")!,
-    supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpZm9nZG9jemxwamx6YXlpZGJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ2MjM3ODEsImV4cCI6MjA3MDE5OTc4MX0.SEvGbEhP6iMVjMb-3hZ3jcVXKdwX6_6tJCGAlV3HSyA"
-)
